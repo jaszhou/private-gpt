9f25d79c3568aee905767b6092cb11bd79582a8b
xiang zhou <jaszhou@gmail.com>
2024-09-27 09:02:55 +1000

    fix update lib script

--- update_lib.sh
+++ update_lib.sh
@@ -1,8 +1,32 @@
 
+
+#!/bin/bash
+
+# This script updates the Lambda function layers
+# Install requirements and create a zip file for the layer
+
+
 pip install -r requirements.txt -t python/
 zip -r layer.zip python
 
-aws lambda publish-layer-version --no-cli-pager --layer-name lambda-layer --zip-file fileb://layer.zip --region ap-southeast-2
+
+v=$(aws lambda publish-layer-version --no-cli-pager --layer-name lambda-layer --zip-file fileb://layer.zip --region ap-southeast-2 --output json --query LayerVersionArn)
+
+# get latest layer version from the output of the publish-layer-version command
+
+echo $v
 
 # careful not override the custom lib for PIL
 # aws lambda update-function-configuration --no-cli-pager --function-name  snapCopyFunction --layers arn:aws:lambda:ap-southeast-2:647207328002:layer:lambda-layer:6
+# aws lambda update-function-configuration --no-cli-pager --function-name  snapCopyFunction --layers arn:aws:lambda:ap-southeast-2:647207328002:layer:lambda-layer:7
+
+aws lambda update-function-configuration --no-cli-pager --function-name  snapCopyFunction --layers "$v"
+
+
+aws lambda update-function-configuration --no-cli-pager --function-name  snapCopyFunction --layers arn:aws:lambda:ap-southeast-2:770693421928:layer:Klayers-p39-pillow:1
+
+
+# ver=`aws lambda publish-layer-version --no-cli-pager --layer-name test-layer --zip-file fileb://layer.zip --region ap-southeast-2 --output json --query LayerVersionArn`
+
+# echo "version: $ver"
+
